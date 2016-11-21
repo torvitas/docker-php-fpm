d918bdcdf0b29d8c31af2b8c9e3b1a5d528cbfee
Sascha Marcel Schmidt <mail@saschaschmidt.net>
2016-11-21 12:31:30 +0100

    ssh-add -l gets called in user entrypoint to make possible shared ids available

--- src/bin/user.entry.sh
+++ src/bin/user.entry.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 export PATH=${PATH}:/usr/local/lib/composer/bin/
+ssh-add -l
 
 if [ -f /usr/local/bin/entrypoint.d/user/*.sh ]; then
     source /usr/local/bin/entrypoint.d/user/*.sh
