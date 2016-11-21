80f9296cff424778d847afc5046d51f9f2bdb07c
Sascha Marcel Schmidt <mail@saschaschmidt.net>
2016-11-21 16:32:12 +0100

    adds deprecation notifications to composer and php lint entrypoints

--- src/bin/entrypoint.sh
+++ src/bin/entrypoint.sh
@@ -32,27 +32,32 @@ case ${1} in
     composer)
         set -e
         disableXDebug
+        echo "using the composer entrypoint is deprecated, this function is going to be removed soon";
         su ${WEB_USER} -pc "composer ${@:2}"
         enableXDebug
         exit 0
         ;;
     composer:create)
         set -e
+        echo "using the composer:create entrypoint is deprecated, this function is going to be removed soon";
         composerCreate
         exit 0
         ;;
     composer:up)
         set -e
+        echo "using the composer:up entrypoint is deprecated, this function is going to be removed soon";
         composerUp
         exit 0
         ;;
     composer:up:dev)
         set -e
+        echo "using the composer:up:dev entrypoint is deprecated, this function is going to be removed soon";
         composerUpDev
         exit 0
         ;;
     phpcs:psr2)
         set -e
+        echo "using the phpcs:psr2 entrypoint is deprecated, this function is going to be removed soon";
         filteredPhpCodeSniffer PSR2
         exit 0
         ;;
